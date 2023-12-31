c34f730e356c975c81b069cb965fc17d9b7f9274
Chibunud <ndubest56@gmail.com>
2024-01-01 00:52:29 +0100

    landing page

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -1,16 +1,24 @@
 const FrontPage = () => {
+  const style = 'border p-1.5 px-5 rounded border-blue-500 font-semibold '
   return (
-    <div>
-      <h1>The world’s first AI-powered tech services company</h1>
-      <p>
-        With over 3 million engineers, Turing uses AI to help businesses build groundbreaking
-        products with our custom application development and on-demand software engineering.
-      </p>
-      <button>Hire Developers</button>
-      <button>Explore Custom Engineering</button>
-      <p>
-        Looking for remote job opportunities? <span>Apply for Jobs</span>
-      </p>
+    <div className='flex'>
+      <div>
+        <h1 className='font-semibold text-5xl leading-snug mb-8'>
+          The world’s first AI-powered tech services company
+        </h1>
+        <p>
+          With over 3 million engineers, Turing uses AI to help businesses build groundbreaking
+          products with our custom application development and on-demand software engineering.
+        </p>
+        <button className={style}>Hire Developers</button>
+        <button className={style}>Explore Custom Engineering</button>
+        <p>
+          Looking for remote job opportunities? <span>Apply for Jobs</span>
+        </p>
+      </div>
+      <div>
+        <img src='./1.JPG' />
+      </div>
     </div>
   )
 }
