6eb44b183f08b78108a3b6268052c39f6f86e180
Chibunud <ndubest56@gmail.com>
2024-01-01 08:27:38 +0100

    business page

--- src/components/MorePage.jsx
+++ src/components/MorePage.jsx
@@ -1,18 +1,38 @@
 const MorePage = () => {
+  const style = 'border mr-3 p-3 px-7 text-lg  rounded border-blue-500 font-semibold '
   return (
-    <div>
-      <div className='w-full mt-10 p-10 bg-gray-50 text-gray-800 text-center items-center '>
+    <>
+      <div className='w-full mt-10 pt-10 pb-8 bg-gray-50 text-gray-800 text-center items-center '>
         <h2 className='text-4xl font-semibold '>
           AI transformation is the new digital transformation
         </h2>
-        <p className='p-10'>
+        <p className='mt-10 mb-2'>
           900+ companies have trusted Turing for their custom engineering and on-demand talent.
         </p>
-        <div className='flex justify-center '>
-          <img src='./1.JPG' className='items-center' />
+        <div className='flex justify-center gap-8 items-center '>
+          <img src='./pepsi.jpg' className='h-20' />
+          <img src='./redit.svg' className='w-28' />
+          <img src='./dell.jpg' className='w-28' />
+          <img src='./volvo.jpg' className='w-28' />
+          <img src='./disnep.jpg' className='w-28' />
         </div>
       </div>
-    </div>
+      <div className='m-20 grid grid-cols-2 gap-16'>
+        <div>
+          <h3 className='text-gray-500 font-bold pb-2'>FOR BUSINESSES</h3>
+          <p className='text-gray-800 font-semibold text-4xl pb-4  leading-snug'>
+            Hire faster with the world's most deeply vetted developers and teams, matched by AI
+          </p>
+          <p className='pb-7 text-gray-800 text-lg'>
+            Quickly and efficiently build your engineering team with our deeply vetted talent.
+          </p>
+          <button className={style + `bg-blue-700  text-slate-200`}>Hire Developers</button>
+        </div>
+        <div>
+          <img src='developers.svg' />
+        </div>
+      </div>
+    </>
   )
 }
 export default MorePage
