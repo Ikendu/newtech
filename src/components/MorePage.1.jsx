c96f195ef89ae3992cc9edbfcb8e4056f3ee46a7
Chibunud <ndubest56@gmail.com>
2024-01-02 00:53:50 +0100

    site analysis

--- src/components/MorePage.1.jsx
+++ src/components/MorePage.1.jsx
@@ -1,6 +1,4 @@
-import { ControlIcon, GlobalIcon, MatchingIcon, PaymentIcon, VettingIcon } from '../assets/Icons'
-
-const MorePage = () => {
+export const MorePage = () => {
   const style = 'border mr-3 p-3 px-7 text-lg  rounded border-blue-500 font-semibold '
   return (
     <>
@@ -69,60 +67,16 @@ const MorePage = () => {
         </div>
       </div>
       <div className='p-20  bg-gray-100'>
-        <h2 className='font-semibold text-5xl text-center text-gray-800 pb-14'>
+        <h2 className='font-semibold text-5xl text-center text-gray-800'>
           Our global reach and AI help you build faster
         </h2>
-        <div className='grid grid-cols-3 gap-10 '>
-          <div className='bg-white rounded-xl mx-auto p-16 mx-4'>
-            <div className='p-2 content-center'>
-              <img src='./a.svg' />
-            </div>
-            <h3 className='font-semibold text-xl mb-2'>We take your specific requirements</h3>
+        <div className='grid grid-cols-3 gap-10'>
+          <div>
+            <img />
+            <h3>We take your specific requirements</h3>
             <ul>
-              <li className='p-2'>Project scope</li>
-              <li className='p-2'>Roles</li>
-              <li className='p-2'>Tech stacks</li>
-              <li className='p-2'>Tech stacks</li>
-              <li className='p-2'>Seniority levels</li>
-              <li className='p-2'>Time zone overlap</li>
-            </ul>
-          </div>
-          <div className='bg-white rounded-xl mx-auto p-16 p-20'>
-            <h3 className='font-semibold text-2xl mb-2 text-center'>
-              And use our global developer platform to handle
-            </h3>
-            <ul className=''>
-              <li className='p-2 bg-blue-50 rounded-xl my-10 font-semibold text-blue-600 text-lg flex gap-2 items-center'>
-                <GlobalIcon />
-                Global Sourcing
-              </li>
-              <li className='p-2 bg-blue-50 rounded-xl  font-semibold text-blue-600 text-lg flex gap-2 items-center'>
-                <VettingIcon />
-                Itelligent Vet ting
-              </li>
-              <li className='p-2 bg-blue-50 rounded-xl my-10 font-semibold text-blue-600 text-lg flex gap-2 items-center'>
-                <MatchingIcon /> Extensive Matching
-              </li>
-              <li className='p-2 bg-blue-50 rounded-xl my-10 font-semibold text-blue-600 text-lg flex gap-2 items-center'>
-                <PaymentIcon /> Payments Compliance
-              </li>
-              <li className='p-2 bg-blue-50 rounded-xl my-10 font-semibold text-blue-600 text-lg flex gap-2 '>
-                <ControlIcon /> Automated On-The-Job Quality Control
-              </li>
-            </ul>
-          </div>
-          <div className='bg-white rounded-xl p-16 mx-4'>
-            <div className='p-2 content-center'>
-              <img src='./a.svg' />
-            </div>
-            <h3 className='font-semibold text-xl mb-2'>We take your specific requirements</h3>
-            <ul>
-              <li className='p-2'>Project scope</li>
-              <li className='p-2'>Roles</li>
-              <li className='p-2'>Tech stacks</li>
-              <li className='p-2'>Tech stacks</li>
-              <li className='p-2'>Seniority levels</li>
-              <li className='p-2'>Time zone overlap</li>
+              <li>Project scope</li>
+              <li />
             </ul>
           </div>
         </div>
@@ -130,4 +84,3 @@ const MorePage = () => {
     </>
   )
 }
-export default MorePage
