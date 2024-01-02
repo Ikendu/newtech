5cdec850e2fd1d8542ca95ad2c3501a09d992979
Chibunud <ndubest56@gmail.com>
2024-01-02 06:02:55 +0100

    site performance page

--- src/components/ReviewPage.jsx
+++ src/components/ReviewPage.jsx
@@ -1,17 +1,24 @@
+import { HeartIcon } from '../assets/Icons'
+
 const ReviewPage = () => {
   return (
-    <div className='bg-gray-200'>
-      <div>
+    <div className='bg-gray-200 p-20'>
+      <div className='grid grid-cols-2 justify-items-end '>
         <div>
-          <h3>People Turing</h3>
-          <p>
+          <h3 className='flex gap-1 text-3xl font-semibold items-center'>
+            People <HeartIcon /> Turing
+          </h3>
+          <p className='text-gray-800 pt-4 text-lg'>
             Read Turing.com reviews from developers across the world and learn what it’s like
             working with top U.S. companies.
           </p>
         </div>
         <div>
-          <h3>4.5OUT OF 5</h3>
-          <p>based on developer reviews as of November 2023</p>
+          <div className='flex gap-2 items-baseline justify-end'>
+            <span className='text-3xl font-semibold '>4.5</span>{' '}
+            <span className='text-xs text-gray-600'>OUT OF 5</span>
+          </div>
+          <p className='text-sm text-gray-600'>based on developer reviews as of November 2023</p>
         </div>
       </div>
       <div></div>
