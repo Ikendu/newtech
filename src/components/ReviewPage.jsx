040fdb0ea0c2b52239fa15518e72df830ab71558
Chibunud <ndubest56@gmail.com>
2024-01-04 17:25:32 +0100

    next slide state function

--- src/components/ReviewPage.jsx
+++ src/components/ReviewPage.jsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
-import { HeartIcon, NextIcon, PrevIcon } from '../assets/Icons'
+import { useEffect, useState } from 'react'
+import { ArrowUpIcon, HeartIcon, NextIcon, PrevIcon } from '../assets/Icons'
 import { slides } from './lists'
 
 const slideIconStyle =
   'absolute top-[50%] p-2 bg-black/20 text-white cursor-pointer rounded-full hidden group-hover:block'
+const indecator = `text-blue-500`
+const indecator1 = `text-gray-700`
 
 const ReviewPage = () => {
   const [currIndex, setCurrIndex] = useState(2)
+
+  const prevSlide = () => {
+    if (currIndex === 0) setCurrIndex(slides.length - 1)
+    else setCurrIndex(currIndex - 1)
+  }
+
+  const nextSlide = () => {
+    const isCurr = currIndex === slides.length - 1
+    const newIndex = isCurr ? 0 : currIndex + 1
+    setCurrIndex(newIndex)
+  }
+
+  const getSlide = (currIndex) => {
+    setCurrIndex(currIndex)
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      nextSlide()
+    }, 3000)
+  }, [nextSlide])
+
   return (
     <div className='bg-gray-200 p-20'>
       <div className='grid grid-cols-2 justify-items-end group-hover:block '>
@@ -30,13 +54,26 @@ const ReviewPage = () => {
       <div className='pt-8 max-w-[700px] h-[400px] w-full m-auto px-4 relative group'>
         <div
           style={{ backgroundImage: `url(${slides[currIndex].url})` }}
-          className='w-full h-full bg-center bg-cover duration-500 rounded-2xl'
-        ></div>
-        <div className={`${slideIconStyle} left-0`}>
+          className='w-full h-full bg-center bg-cover  duration-400 rounded-2xl  '
+        >
+          hello we can write here
+        </div>
+        <div className={`${slideIconStyle} left-0`} onClick={prevSlide}>
           <PrevIcon />
         </div>
-        <div className={`${slideIconStyle} right-0`}>
+        <div className={`${slideIconStyle} right-0`} onClick={nextSlide}>
           <NextIcon />
+        </div>
+        <div className={`flex text-gray-500 gap-5 m-auto justify-center`}>
+          {slides.map((slide, indx) => (
+            <div
+              key={indx}
+              onClick={() => getSlide(indx)}
+              className={currIndex === indx ? indecator : indecator1}
+            >
+              <ArrowUpIcon />
+            </div>
+          ))}
         </div>
       </div>
     </div>
